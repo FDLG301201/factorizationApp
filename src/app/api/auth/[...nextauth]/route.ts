5726079cb276b177dbab2526240a1919f53f2a63
FDLG301201 <fdlg301201@gmail.com>
2025-05-12 22:48:22 -0400

    fix: next-auth secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -54,7 +54,8 @@ const handler = NextAuth({
           }
           return session;
         }
-      }
+      },
+      secret: process.env.NEXTAUTH_SECRET,
 })
 
 export { handler as GET, handler as POST }
